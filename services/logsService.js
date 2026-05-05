716bd292eec0422a757acc2d5495b7492b5ff3c1
Alik <alik.wansiedler@stud.hshl.de>
2026-05-05 18:39:52 +0200

    Baisc routes for Login and Logs

--- services/logsService.js
+++ services/logsService.js
@@ -1,1 +1,8 @@
 // Handling for the data
+const db = require("../database/db");
+
+exports.getLogs = async () => {
+  const result = await db.query(`SELECT * FROM windows_logs`);
+
+  return result.rows;
+};
