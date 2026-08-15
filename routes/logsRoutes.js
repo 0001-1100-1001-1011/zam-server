26ae6c5fc6febf6a3b4d3a52cd730e31a7b1e0ff
MarcoEbel <marco-ebel-91@web.de>
2026-08-15 16:36:49 +0200

    added Hmac Import and Handling for Logsroutes

--- routes/logsRoutes.js
+++ routes/logsRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const controller = require("../controllers/logsController");
+const hmac = require("../middleware/hmac");
 
 // get logs
 router.get("/logs", controller.getLogs);
-router.post("/logs", controller.postLogs);
+router.post("/logs", hmac, controller.postLogs);
 
 module.exports = router;
