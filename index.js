b885be8c6f426ed21fde4e3125a54024b6342147
Alik Wansiedler <164514709+0001-1100-1001-1011@users.noreply.github.com>
2026-08-16 19:58:19 +0200

    Rename softwareRoutes to softwaresRoutes
    modified name

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/auth", require("./routes/signupRoutes"));
 app.use("/auth", require("./routes/loginRoutes"));
 app.use("/api", require("./routes/logsRoutes"));
 app.use("/api", require("./routes/hostsRoutes"));
-app.use("/api", require("./routes/softwareRoutes"));
+app.use("/api", require("./routes/softwaresRoutes"));
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf Port ${PORT}`);
