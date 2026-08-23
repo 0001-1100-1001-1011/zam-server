abe7b578f5b8e7fea0e166fd0dfd4c17a5a7a8c1
zoiska <zoisk64@gmail.com>
2026-08-24 01:16:51 +0200

    naming

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,10 @@
-import { loginService, createAcessToken, createRefreshToken } from "../services/loginService.js";
+import { loginService, createAccessToken, createRefreshToken } from "../services/loginService.js";
 
 export async function userLogin(req, res) {
   try {
     const user = await loginService(req.body);
 
-    const accessToken = await createAcessToken(user);
+    const accessToken = await createAccessToken(user);
     const refreshToken = await createRefreshToken(user);
 
     res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken });
--- controllers/refreshController.js
+++ controllers/refreshController.js
@@ -1,9 +1,9 @@
 import { refreshService } from "../services/refreshService.js";
 
-export async function refreshController(token) {
+export async function refreshController(data) {
   try {
-    const freshToken = await refreshService(token);
-    res.status(200).json({ refreshToken: freshToken });
+    const refreshToken = await refreshService(data);
+    res.status(200).json({ refreshToken: refreshToken });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
     throw new Error(error.message);
