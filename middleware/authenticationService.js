a221443e2fbb0b19944493c33c83e6b275a0a516
zoiska <zoisk64@gmail.com>
2026-08-23 05:03:54 +0200

    check for bearer, correct algorithm

--- middleware/authenticationService.js
+++ middleware/authenticationService.js
@@ -1,13 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export default function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.status(401).json({ error: "No token provided" });
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer ") || token == null)
+    return res.status(401).json({ error: "No token provided" });
+  const token = authHeader.split(" ")[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: "Forbidden" });
-    req.user = user;
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, { algorithms: ["HS256"] }, (err, payload) => {
+    if (err) return res.status(401).json({ error: "Token invalid" });
+    req.user = payload;
     next();
   });
 }
