ba14090007c43f7b4210df04fef168eac854fb1f
zoiska <zoisk64@gmail.com>
2026-08-24 01:14:07 +0200

    fix bug calling var before declaration

--- middleware/authenticationService.js
+++ middleware/authenticationService.js
@@ -2,9 +2,10 @@ import jwt from "jsonwebtoken";
 
 export default function authenticateToken(req, res, next) {
   const authHeader = req.headers.authorization;
-  if (!authHeader?.startsWith("Bearer ") || token == null)
+  if (!authHeader?.startsWith("Bearer "))
     return res.status(401).json({ error: "No token provided" });
   const token = authHeader.split(" ")[1];
+  if (token == null) return res.status(401).json({ error: "No token provided" });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, { algorithms: ["HS256"] }, (err, payload) => {
     if (err) return res.status(401).json({ error: "Token invalid" });
