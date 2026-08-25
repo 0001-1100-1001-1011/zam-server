c0c1cd004444e22e0d1a513e5b0462b73b1dcdfe
zoiska <zoisk64@gmail.com>
2026-08-26 01:37:05 +0200

    call service for new jwt token pair and send them back

--- controllers/refreshController.js
+++ controllers/refreshController.js
@@ -2,10 +2,19 @@ import { refreshService } from "../services/refreshService.js";
 
 export async function refreshController(req, res) {
   try {
-    const refreshToken = await refreshService(req.body);
-    res.status(200).json({ refreshToken: refreshToken });
+    const { accessToken, refreshToken } = await refreshService(req);
+    res
+      .cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false, // muss bei umstieg auf HTTPS auf true gesetzt werden!
+        sameSite: "lax", // kann mit proxy eig auf "strict" gesetzt werden für maximale cross-site-request-forgery protection
+        path: "/auth/refresh",
+        maxAge: 60 * 60 * 24 * 30 * 1000, // in Millikunden, auf 30 Tage
+      })
+      .status(200)
+      .json({ accessToken: accessToken });
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(401).json({ message: "Unauthorized" });
     throw new Error(error.message);
   }
 }
