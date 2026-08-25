3e5ed8d65a30255212899492bc038bd315b72509
zoiska <zoisk64@gmail.com>
2026-08-26 01:37:30 +0200

    fix reversed params

--- middleware/noCache.js
+++ middleware/noCache.js
@@ -1,4 +1,4 @@
-export async function noCache(res, req, next) {
+export async function noCache(req, res, next) {
   res.header("Cache-Control", "private, no-cache, no-store");
   res.header("Pragma", "no-cache");
   next();
