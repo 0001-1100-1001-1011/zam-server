d3378ce1ec0a7cd0f7ffec5b7e7b234786f5816b
zoiska <zoisk64@gmail.com>
2026-08-25 03:55:48 +0200

    fix cookie path

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -12,7 +12,7 @@ export async function userLogin(req, res) {
         httpOnly: true,
         secure: false, // muss bei umstieg auf HTTPS auf true gesetzt werden!
         sameSite: "lax", // kann mit proxy eig auf "strict" gesetzt werden für maximale cross-site-request-forgery protection
-        path: "/api/refresh",
+        path: "/auth/refresh",
         maxAge: 60 * 60 * 24 * 30, // in Sekunden, auf 30 Tage
       })
       .status(200)
