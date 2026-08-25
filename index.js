e46cea4a62c3d5a927cc717e923ad30a4afd8522
zoiska <zoisk64@gmail.com>
2026-08-26 01:41:49 +0200

    add cors policy

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ import "./cronjob/dailyCVE.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(cookieParser());
 
