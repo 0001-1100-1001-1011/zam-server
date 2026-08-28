ff6a1491d9aee4efbe2cd919d9b3954dbcd1d7c9
MarcoEbel <marco-ebel-91@web.de>
2026-08-29 01:50:37 +0200

    removed POST logic, moved to agentSoftwaresController

--- controllers/softwaresController.js
+++ controllers/softwaresController.js
@@ -1,4 +1,4 @@
-import { getSoftwareService, postSoftwareService } from "../services/softwaresService.js";
+import { getSoftwareService} from "../services/softwaresService.js";
 
 export async function getSoftware(req, res) {
   try {
@@ -7,16 +7,5 @@ export async function getSoftware(req, res) {
   } catch (error) {
     console.error("Fehler beim Abrufen der Softwares:", error);
     res.status(500).json({ error: "Internal Server Error" });
-  }
-}
-
-export async function postSoftware(req, res) {
-  try {
-    await postSoftwareService(req.body);
-    console.log("Softwares created");
-    res.status(201).json({ message: "created" });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ error: error.message });
   }
 }
