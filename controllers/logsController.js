3bd902bb4395640c59a4e5c679e99e1b60aa5bcd
MarcoEbel <marco-ebel-91@web.de>
2026-08-29 01:50:17 +0200

    removed POST logic, moved to agentLogsController

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,4 +1,4 @@
-import { getLogsService, postLogsService } from "../services/logsService.js";
+import { getLogsService} from "../services/logsService.js";
 
 export async function getLogs(req, res) {
   try {
@@ -7,16 +7,5 @@ export async function getLogs(req, res) {
   } catch (error) {
     console.error("Fehler beim Abrufen der Logs:", error);
     res.status(500).json({ error: "Internal Server Error" });
-  }
-}
-
-export async function postLogs(req, res) {
-  try {
-    await postLogsService(req.body);
-    console.log("Logs created");
-    res.status(201).json({ message: "created" });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ error: error.message });
   }
 }
