bfa01e8ced549da721aca5568054db833d77abf5
zoiska <zoisk64@gmail.com>
2026-08-29 18:16:30 +0200

    add validation function and related schema

--- routes/agentLogsRoutes.js
+++ routes/agentLogsRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import hmac from "../middleware/hmac.js";
 import { postAgentLogs } from "../controllers/agentLogsController.js";
+import { inputValidation } from "../middleware/inputValidation.js";
+import { agentLogsSchema } from "../schemas/agentValidationSchema.js";
 
 const router = express.Router();
 
-router.post("/logs", hmac, postAgentLogs);
+router.post("/logs", hmac, inputValidation(agentLogsSchema), postAgentLogs);
 
 export default router;
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { userLogin } from "../controllers/loginController.js";
+import { inputValidation } from "../middleware/inputValidation.js";
+import { monitoringUserLoginSchema } from "../schemas/agentValidationSchema.js";
 
 const router = express.Router();
 
-router.post("/login", userLogin);
+router.post("/login", inputValidation(monitoringUserLoginSchema), userLogin);
 
 export default router;
